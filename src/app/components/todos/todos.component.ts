456aaff9e7171568ec4e77403194b6a3cbf70d6f
Monesh Goyal <goyalmonesh2201@gmail.com>
2022-09-27 23:47:04 +0530

    hosting the todo app

---
+++ src/app/components/todos/todos.component.ts
@@ -0,0 +1,50 @@
+import { Component, OnInit } from '@angular/core';
+import { Todo } from 'src/app/models/Todo';
+
+@Component({
+  selector: 'app-todos',
+  templateUrl: './todos.component.html',
+  styleUrls: ['./todos.component.css']
+})
+export class TodosComponent implements OnInit {
+
+  todos !: Todo[];
+
+  inputTodo: string = '';
+
+  constructor() { }
+
+  ngOnInit(): void {
+    this.todos = [
+      {
+        content : 'First Todo',
+        completed : false
+      },
+      {
+        content : "Second Todo",
+        completed : false
+      }
+    ]
+  }
+
+  toggleDone(id: number){
+    this.todos.map((v, i) => {
+      if(i == id) 
+      v.completed = !v.completed;
+      return v;
+    })
+  }
+
+  deleteTodo (id: number){
+    this.todos = this.todos.filter((v, i) => i !== id);
+  }
+
+  addTodo() {
+    this.todos.push({
+      content: this.inputTodo,
+      completed: false
+    })
+    this.inputTodo = "";
+  }
+
+}
